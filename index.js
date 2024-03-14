6125009bb1160873fc18dbdec98e62f7ab5095e6
Anurag sinha <117573186+Anuragsinha05@users.noreply.github.com>
2024-03-14 20:51:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import App2 from './App2';
+import SpreadOperator from './SpreadOperator'
+
+ ReactDOM.render(
+    <SpreadOperator />,document.getElementById('root')
+);
+
+
+
